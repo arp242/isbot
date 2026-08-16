b59493631614bfd550abf0c9012705131163b604
Martin Tournoij <martin@arp242.net>
2026-08-16 23:10:05 +0100

    Expand list of cloud IP addresses
    I haven't updated the script to fetch from RIPE/ARIN in quite a few
    years, and it's ugly and doesn't work. Instead, use:
    https://github.com/rezmoss/cloud-provider-ip-addresses
    
    Because the list is now a lot longer, using just a slice is too slow:
    ~40,000ns for the worst case, instead of ~3,800ns. So store as a map of
    the first byte (or first 2 for IPv6). This makes it consistently ~700ns.
    
    Storing it as a map directly bloats the compiles binary by about 15M, so
    parse it from text on start-up. That's fast enough so should be okay.

--- ip.go
+++ ip.go
@@ -1,9 +1,10 @@
-//go:generate ./mkip
+//go:generate go run ./cmd/iprange
 
 package isbot
 
 import (
 	"net/netip"
+	"strings"
 )
 
 type ipRange struct {
@@ -11,13 +12,60 @@ type ipRange struct {
 	prefix netip.Prefix
 }
 
-func parseNet(ip string, b Result) ipRange {
-	prefix, err := netip.ParsePrefix(ip)
-	if err != nil {
-		panic(err)
+func botname(n string) Result {
+	switch n {
+	case "AWS":
+		return BotRangeAWS
+	case "DigitalOcean":
+		return BotRangeDigitalOcean
+	case "ServersCom":
+		return BotRangeServersCom
+	case "GoogleCloud":
+		return BotRangeGoogleCloud
+	case "Hetzner":
+		return BotRangeHetzner
+	case "Azure":
+		return BotRangeAzure
+	case "Alibaba":
+		return BotRangeAlibaba
+	case "Linode":
+		return BotRangeLinode
+	case "Oracle":
+		return BotRangeOracle
+	case "OVH":
+		return BotRangeOVH
 	}
-	return ipRange{bot: b, prefix: prefix}
+	panic(n)
 }
+
+var ipRanges4 = func() map[byte][]ipRange {
+	m := make(map[byte][]ipRange)
+	for f := range strings.FieldsSeq(ranges4) {
+		ip, name, ok := strings.Cut(f, ",")
+		if !ok {
+			panic(f)
+		}
+		prefix := netip.MustParsePrefix(ip)
+		k := prefix.Addr().As4()[0]
+		m[k] = append(m[k], ipRange{prefix: prefix, bot: botname(name)})
+	}
+	return m
+}()
+
+var ipRanges6 = func() map[[2]byte][]ipRange {
+	m := make(map[[2]byte][]ipRange)
+	for f := range strings.FieldsSeq(ranges6) {
+		ip, name, ok := strings.Cut(f, ",")
+		if !ok {
+			panic(f)
+		}
+		prefix := netip.MustParsePrefix(ip)
+		as := prefix.Addr().As16()
+		k := [2]byte{as[0], as[1]}
+		m[k] = append(m[k], ipRange{prefix: prefix, bot: botname(name)})
+	}
+	return m
+}()
 
 // IPRange checks if this IP address is from a range that should normally never
 // send browser requests, such as AWS and other cloud providers.
@@ -30,7 +78,15 @@ func IPRange(addr string) Result {
 		return NoBotKnown
 	}
 
-	for _, r := range ipRanges {
+	var ranges []ipRange
+	if ip.Is4() {
+		ranges = ipRanges4[ip.As4()[0]]
+	} else {
+		as := ip.As16()
+		ranges = ipRanges6[[2]byte{as[0], as[1]}]
+	}
+
+	for _, r := range ranges {
 		if r.prefix.Contains(ip) {
 			return r.bot
 		}
